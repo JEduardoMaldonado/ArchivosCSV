f280a41b7a3de27fb10f67136e38f5f3f848a5eb
JEduardoMaldonado <joseeduardocruzmaldonado@gmail.com>
2016-10-14 18:29:04 -0500

    archivosCSV
    archivosCSV

---
+++ menu.py
@@ -0,0 +1,30 @@
+# coding utf-8
+import csv
+import sys
+
+print "----------------MENÚ---------------"
+print "1. INSERTAR"
+print "2. LEER"
+print "3. SALIR"
+
+opcion=int(input("Elige una opcion "))
+
+if opcion == 1:
+    nombre = raw_input("Ingresa Nombre: ")
+    email = raw_input("Ingresa Email: ")
+    with open ('data.csv','a') as file:
+        dat = csv.writer(file, delimiter=",")
+        dat.writerow([''+str(nombre), ''+(email)])
+
+if opcion == 2:
+    with open ('data.csv', 'r') as file:
+        data = csv.reader(file, delimiter=',')
+        for line in data:
+            print line
+
+if opcion == 3:
+    sys.exit(0)    
+
+
+
+
